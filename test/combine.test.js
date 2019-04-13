b62ac5501d59a033ee8bf2f12d143d4b9f528ec6
Matthew Tylee Atkinson <matatk@gmail.com>
2019-04-13 23:38:59 +0100

    fix: Make the changelog header sentence case (as far as we can) for consistency

--- test/combine.test.js
+++ test/combine.test.js
@@ -9,7 +9,7 @@ First lot of awesome stuff!
 
 `
 
-	const expected = `# Wibble Changelog
+	const expected = `# Wibble changelog
 
 ## 0.1.0
 
@@ -25,7 +25,7 @@ First lot of awesome stuff!
 })
 
 tap.test('when there is existing loggage', t => {
-	const existingLog = `# Landmarks Changelog
+	const existingLog = `# Landmarks changelog
 
 ## [2.5.5](https://github.com/matatk/landmarks/compare/2.5.4...2.5.5) (2019-03-25)
 
@@ -38,7 +38,7 @@ Awesome new stuff!
 
 `
 
-	const expectedCombined = `# Landmarks Changelog
+	const expectedCombined = `# Landmarks changelog
 
 ## [2.5.6](https://github.com/matatk/landmarks/compare/2.5.5...2.5.6) (2019-04-13)
 
